3efe2ed4d779cf558386dcb647e85f93e9483cbe
Martin Peth <martin@peth.pl>
2025-02-24 13:19:01 +0100

    chore: moved files to a new location

--- src/CLIManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "CLIManager.h"
-
-void CLIManager::registerCommand(std::shared_ptr<Command> command) {
-    commands[command->getName()] = command;
-}
-
-void CLIManager::begin() {
-    String deviceInputTopic = String(mqttManager.getDeviceTopic()) + String("/cli");
-}
--- src/CommandSource.cpp
+++
@@ -1,9 +0,0 @@
-#include "CommandSource.h"
-
-void SerialCommandSource::sendResponse(const char* response) {
-    Serial.println(response);
-}
-
-void MQTTCommandSource::sendResponse(const char* response) {
-    mqttManager.publish(topic, response);
-}
