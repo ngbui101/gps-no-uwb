078ce2e9bd038b6e76538c8b55c1e96630b4e659
Martin Peth <martin@peth.pl>
2025-03-14 20:26:35 +0100

    feat(log): HH:MM:SS timestamp and output formatting

--- src/managers/LogManager.cpp
+++ src/managers/LogManager.cpp
@@ -1,51 +1,76 @@
 #include "managers/LogManager.h"
 
-const char* LogManager::LOG_TOPIC_PREFIX = "devices";
+const char *LogManager::LOG_TOPIC_PREFIX = "devices";
 
-const char* LogManager::getLogLevelString(LogLevel level) {
-    switch (level) {
-        case LogLevel::DEBUG: return "DEBUG";
-        case LogLevel::INFO: return "INFO";
-        case LogLevel::WARNING: return "WARNING";
-        case LogLevel::ERROR: return "ERROR";
-        default: return "UNKNOWN";
+const char *LogManager::getLogLevelString(LogLevel level)
+{
+    switch (level)
+    {
+    case LogLevel::DEBUG:
+        return "DEBUG";
+    case LogLevel::INFO:
+        return "INFO";
+    case LogLevel::WARNING:
+        return "WARNING";
+    case LogLevel::ERROR:
+        return "ERROR";
+    default:
+        return "UNKNOWN";
     }
 }
 
-bool LogManager::isLogLevelEnabled(LogLevel level) {
+bool LogManager::isLogLevelEnabled(LogLevel level)
+{
     return static_cast<int>(level) >= static_cast<int>(logLevel);
 }
 
-void LogManager::log(LogLevel level, const char* source, const char* message) {
-    if (static_cast<int>(logLevel) < 0 || static_cast<int>(logLevel) > getLogLevelCount()-1) {
+void LogManager::log(LogLevel level, const char *source, const char *message)
+{
+    if (static_cast<int>(logLevel) < 0 || static_cast<int>(logLevel) > getLogLevelCount() - 1)
+    {
         logLevel = LogLevel::INFO;
         return;
     }
 
-    if (!isLogLevelEnabled(level)) {
+    if (!isLogLevelEnabled(level))
+    {
         return;
     }
 
-    unsigned long ms = millis();
+    const unsigned long ms = millis();
+    unsigned long totalSeconds = ms / 1000;
+    const unsigned int hours = totalSeconds / 3600;
+    const unsigned int minutes = (totalSeconds % 3600) / 60;
+    const unsigned int seconds = totalSeconds % 60;
 
     char msgBuffer[256];
-    snprintf(msgBuffer, sizeof(msgBuffer), "[%lu] %s: %s: %s", ms, getLogLevelString(level), source, message);
+    snprintf(msgBuffer, sizeof(msgBuffer), "[%02u:%02u:%02u] -- %-7s| %-15s: %s",
+             hours,
+             minutes,
+             seconds,
+             getLogLevelString(level),
+             source,
+             message);
 
     Serial.println(msgBuffer);
 }
 
-void LogManager::debug(const char* source, const char* message) {
+void LogManager::debug(const char *source, const char *message)
+{
     log(LogLevel::DEBUG, source, message);
 }
 
-void LogManager::info(const char* source, const char* message) {
+void LogManager::info(const char *source, const char *message)
+{
     log(LogLevel::INFO, source, message);
 }
 
-void LogManager::warning(const char* source, const char* message) {
+void LogManager::warning(const char *source, const char *message)
+{
     log(LogLevel::WARNING, source, message);
 }
 
-void LogManager::error(const char* source, const char* message) {
+void LogManager::error(const char *source, const char *message)
+{
     log(LogLevel::ERROR, source, message);
 }
